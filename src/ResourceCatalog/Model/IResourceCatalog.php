da493f84552bcc96aa13d7f039e914573d1c7a8b
houyuting <956173562@qq.com>
2020-11-24 17:00:44 +0800

    #133246-msg-修改公共资源目录

--- src/ResourceCatalog/Model/IResourceCatalog.php
+++ src/ResourceCatalog/Model/IResourceCatalog.php
@@ -423,6 +423,16 @@ interface IResourceCatalog extends IResourceCatalogTypeName, IResourceCatalogTyp
             self::TYPE['HEIMDXX'],
             self::TYPE['HONGMDXX'],
             self::TYPE['HYFJFLJG'],
+            self::TYPE['XYTXHMD'],
+            self::TYPE['XZQZXX'],
+            self::TYPE['XZZSXX'],
+            self::TYPE['XZCJXX'],
+            self::TYPE['XZQRXX'],
+            self::TYPE['XZJFXX'],
+            self::TYPE['JDJCXX'],
+            self::TYPE['XZJLXX'],
+            self::TYPE['QTXZZFXX'],
+            self::TYPE['QYHYSJ']
         ),
         IUserGroup::ID['STHJJ'] => array(
             self::TYPE['HEIMDXX'],
